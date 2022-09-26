f9de6da26de0c1f8b368b9989ee1fb5eda7bf184
Jack Meng <jackmeng0814@gmail.com>
2022-09-26 18:30:30 -0400

    `add` json-generator

--- pkg/src/jackmeng/Parser.java
+++ pkg/src/jackmeng/Parser.java
@@ -15,25 +15,11 @@ public class Parser {
   static final int NOV = 11,
       DEC = 12, USOPEN = 4, MAR = 3, JAN = 1, FEB = 2;
 
-  /**
-   * Parses the formatted line into a JSON element
-   * with the leading key being the line number
-   * found.
-   *
-   * @param line
-   * @return
-   */
-  public static String parse1(int lNum, String line) {
-    String[] split = line.split(" ");
-    return "\"q_" + Math.abs(lNum) + " \": { \"url\":\"" + split[0] + "\", \"year\":" + split[1] + ",\n\"month\":\""
-        + split[2] + "\",\n\"qnum\":\""
-        + split[3] + "\",\n\"qname\": \"" + split[4].replace(ATTRIBUTES_DELIMITER, " ") + "\"},";
-  }
-
   public static String parse2(String line) {
     String[] split = line.split(" ");
-    return "{ \"url\":\"" + split[0] + "\", \"year\":" + split[1] + ",\n\"qnum\":\""
-        + split[3] + "\",\n\"qname\": \"" + split[4].replace(ATTRIBUTES_DELIMITER, " ") + "\"},";
+    return "{ \"url\":\"" + split[0] + "\", \"div\":\"" + split[1] + "\",\n\"year\":\""
+        + split[2] + "\",\n\"month\": \"" + getMonth(line) + "\",\n\"qnum\":\"" + split[4] + "\",\n\"name\":\""
+        + split[5].replace(ATTRIBUTES_DELIMITER, " ") + "\"},";
   }
 
   public static int getYear(String line) {
@@ -64,17 +50,23 @@ public class Parser {
       MAXIM = Integer.parseInt(args[1]);
       ATTRIBUTES_DELIMITER = args[2];
     }
-    if (new File("bin/plat.json").exists()) {
-      File f = new File("bin/plat.json");
+    if (new File("bin/rnd.json").exists()) {
+      File f = new File("bin/rnd.json");
       f.delete();
       f.createNewFile();
     }
     try (BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream("bin/scraper.txt")));
-        PrintWriter pw2 = new PrintWriter("bin/plat.json")) {
-      pw2.println("{\n");
+        PrintWriter pw2 = new PrintWriter("bin/rnd.json")) {
+      pw2.println("[\n");
       String curr = br.readLine();
-          
-      pw2.println("\n}");
+      while (curr != null) {
+        curr = br.readLine();
+        if (curr != null) {
+          pw2.println(parse2(curr));
+          pw2.flush();
+        }
+      }
+      pw2.println("\n]");
     }
   }
 }
