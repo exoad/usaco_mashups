1d0c54171362c681a43f5541e32ad8ab8020af1d
Jack Meng <jackmeng0814@gmail.com>
2022-09-26 23:00:27 -0400

    `update` master sheet

---
+++ pkg/jackmeng/commands/general/idhelp.js
@@ -0,0 +1,25 @@
+const { EmbedBuilder } = require("discord.js");
+// @ts-ignore
+const colors = require("../../../../configs/colors.json");
+// @ts-ignore
+const app = require("../../../../configs/bot.json");
+const { Database } = require("secure-db");
+// @ts-ignore
+const manifest = require("../../../../internal/MANIFEST.json");
+
+module.exports = {
+  config: {
+    name: "idk",
+    category: "General",
+    description: "Get information on how problem ID works for this bot",
+    usage: app.strings.arguments_null,
+    aliases: [`idsystem`, `idhelp`],
+  },
+  run: async (bot, msg, args) => {
+    const embed = new EmbedBuilder()
+      .setTitle("ID System README")
+      .setDescription("```" + app.strings.id_system_readme + "```");
+
+    msg.channel.send({ embeds: [embed] });
+  },
+};
--- pkg/src/jackmeng/Parser.java
+++ pkg/src/jackmeng/Parser.java
@@ -42,6 +42,27 @@ public class Parser {
     return line.split(" ")[0].split("cpid=")[1];
   }
 
+  public static String parseMonthName(String str) {
+    str = str.toLowerCase();
+    return str.startsWith("jan") ? "jan"
+        : str.startsWith("feb") ? "feb" : str.startsWith("uso") ? "opn" : str.startsWith("mar") ? "mar" : "jan";
+  }
+
+  public static String parseDivName(String str) {
+    str = str.toLowerCase();
+    return str.startsWith("bro") ? "brnz"
+        : str.startsWith("silv") ? "silv" : str.startsWith("plat") ? "plat" : str.startsWith("gol") ? "gold" : "brnz";
+  }
+
+  public static String parseQNum(String str) {
+    return str.contains("3") ? "3" : str.contains("4") ? "4" : str.contains("1") ? "1" : str.contains("2") ? "2" : "1";
+  }
+
+  public static String getID(String line) {
+    return line.split(" ")[2] + parseMonthName(line.split(" ")[3]) + parseDivName(line.split(" ")[1])
+        + parseQNum(line.split(" ")[4]);
+  }
+
   /**
    * Required arguments for the parser to work
    *
@@ -68,12 +89,36 @@ public class Parser {
       f.delete();
       f.createNewFile();
     }
+    if (new File("bin/map_master.json").exists()) {
+      File f = new File("bin/map_master.json");
+      f.delete();
+      f.createNewFile();
+    }
+    if (new File("bin/map_slave.json").exists()) {
+      File f = new File("bin/map_slave.json");
+      f.delete();
+      f.createNewFile();
+    }
     try (BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream("bin/scraper.txt")));
-        PrintWriter pw2 = new PrintWriter("bin/rnd.json"); PrintWriter urls = new PrintWriter("bin/urls.json"); PrintWriter cpids = new PrintWriter("bin/cpids.json")) {
+        PrintWriter pw2 = new PrintWriter("bin/rnd.json");
+        PrintWriter urls = new PrintWriter("bin/urls.json");
+        PrintWriter cpids = new PrintWriter("bin/cpids.json");
+        PrintWriter mapm = new PrintWriter("bin/map_master.json");
+        PrintWriter maps = new PrintWriter("bin/map_slave.json");) {
       pw2.println("[\n");
       urls.println("[\n");
       cpids.println("[\n");
+      mapm.println("{\n");
+      maps.println("{\n");
       String curr = br.readLine();
+      pw2.println(parse2(curr));
+      pw2.flush();
+      urls.println("\"" + getURL(curr) + "\",");
+      urls.flush();
+      cpids.println(getCPID(curr) + ",");
+      cpids.flush();
+      mapm.println("\"" + getCPID(curr) + "\":" + "\"" + getID(curr) + "\",");
+      mapm.flush();
       while (curr != null) {
         curr = br.readLine();
         if (curr != null) {
@@ -83,11 +128,14 @@ public class Parser {
           urls.flush();
           cpids.println(getCPID(curr) + ",");
           cpids.flush();
+          mapm.println("\"" + getID(curr) + "\":" + "\"" + getCPID(curr) + "\",");
+          mapm.flush();
         }
       }
       pw2.println("\n]");
       urls.println("\n]");
       cpids.println("\n]");
+      mapm.println("\n}");
     }
 
   }
