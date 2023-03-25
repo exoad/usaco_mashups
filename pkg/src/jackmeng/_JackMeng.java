fa05fb29e986df65ebb268da8627132cb823949d
Jack Meng <jackmeng0814@gmail.com>
2023-03-25 19:40:56 -0400

    `add` idk

--- pkg/src/jackmeng/_JackMeng.java
+++ pkg/src/jackmeng/_JackMeng.java
@@ -1,10 +1,14 @@
 package jackmeng;
 
+import javax.swing.*;
+
 public class _JackMeng
 {
   static void run_Gui()
   {
+    JFrame jf = new JFrame("USACO_MashUp Bot");
 
+    
   }
 
   public static void main(String[] args)
