d5e57ccb019842a0e70613eafa48438c9fa6f7e9
Jack Meng <jackmeng0814@gmail.com>
2023-11-23 13:27:42 -0500

    `fix`

--- pkg/jackmeng/commands/general/contesttime.js
+++ pkg/jackmeng/commands/general/contesttime.js
@@ -1,15 +1,10 @@
-/**
- */
-
+const { NodeHtmlMarkdown } = require("node-html-markdown");
 const { EmbedBuilder } = require("discord.js");
 // @ts-ignore
-const colors = require("../../../../configs/colors.json");
 // @ts-ignore
-const app = require("../../../../configs/bot.json");
 const { Database } = require("secure-db");
 // @ts-ignore
 const manifest = require("../../../../internal/MANIFEST.json");
-const superagent = require("superagent");
 const { schedule } = require("../../../../bin/usaco.json");
 
 module.exports = {
@@ -20,7 +15,7 @@ module.exports = {
     usage: "No argument accepted",
     aliases: [`contests`],
   },
-  run: async (bot, msg, args) => {
+  run: async (_, msg, __) => {
     let id = msg.author.id;
     const bldb = new Database(manifest["blacklisted-registry"]);
     if (bldb.has(id)) {
@@ -28,10 +23,11 @@ module.exports = {
     } else {
       const embed = new EmbedBuilder()
         .setTitle("USACO Contest Schedule")
-        .setDescription(!schedule ? "Unable to retrieve reqest" : schedule)
-        .setFooter({
-          text: "[!!] These dates and times may or may not up to date, take it with a grain of salt [!!]",
-        })
+        .setDescription(
+          !schedule
+            ? "Unable to retrieve request"
+            : NodeHtmlMarkdown.translate(schedule)
+        )
         .setColor("Random");
       msg.channel.send({ embeds: [embed] });
     }
--- pkg/src/jackmeng/run_ContestTime.java
+++ pkg/src/jackmeng/run_ContestTime.java
@@ -10,21 +10,22 @@ import org.jsoup.select.Elements;
 public class run_ContestTime
 {
 
-  static final String USACO_URL = "http://www.usaco.org/";
+  static final String USACO_URL="http://www.usaco.org/";
 
   public static String readURL(String url)
   {
-    StringBuilder sb = new StringBuilder();
-    String curr = "";
-    try (BufferedReader br = new BufferedReader(new InputStreamReader(new URL(url).openStream())))
+    StringBuilder sb=new StringBuilder();
+    String curr="";
+    try(BufferedReader br=new BufferedReader(new InputStreamReader(new URL(url).openStream())))
     {
       sb.append(br.readLine());
-      while (curr != null)
+      while(curr!=null)
       {
-        curr = br.readLine();
-        sb.append("\n").append(curr);
+        curr=br.readLine();
+        sb.append("\n")
+          .append(curr);
       }
-    } catch (Exception e)
+    } catch(Exception e)
     {
       e.printStackTrace();
     }
@@ -33,12 +34,22 @@ public class run_ContestTime
 
   public static void main(String... args) throws Exception
   {
-    Document dc = Jsoup.parse(readURL(USACO_URL));
-    Elements schedule = dc.select(".content > div:nth-child(5) > div:nth-child(3)");
-    try (PrintWriter pw = new PrintWriter("bin/usaco.json"))
+    Document dc=Jsoup.parse(readURL(USACO_URL));
+    Elements schedule=dc.select(".content > div:nth-child(5) > div:nth-child(3)");
+    try(PrintWriter pw=new PrintWriter("bin/usaco.json"))
     {
-      pw.println("{\n\"schedule\":\""
-          + schedule.html().replace("<br>", "\\n").replace("<h2>", "**").replace("</h2>", "**\\n") + "\"\n}");
+      pw.println(
+          "{\n\"schedule\":\""
+              +schedule.html()
+                       .replace(
+                           "<h2>",
+                           "<strong>"
+                       )
+                       .replace(
+                           "</h2>",
+                           "</strong><br/>"
+                       )+"\"\n}"
+      );
       pw.flush();
     }
   }
